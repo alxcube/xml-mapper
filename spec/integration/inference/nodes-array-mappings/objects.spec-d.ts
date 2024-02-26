093933ad78f34e1adcc596d58433405121434c44
Alexandr Alexandrov <alxcube@gmail.com>
2024-02-27 00:04:48 +0200

    Added type inference tests for  array of objects mappings

---
+++ spec/integration/inference/nodes-array-mappings/objects.spec-d.ts
@@ -0,0 +1,126 @@
+import { describe, expectTypeOf, test } from "vitest";
+import {
+  map,
+  type ObjectBlueprint,
+  type SingleNodeDataExtractorFn,
+} from "../../../../src";
+
+declare function toJson(obj: object): string;
+describe("mapping to object inference", () => {
+  interface TestObject {
+    str: string;
+    num: number;
+    bool: boolean;
+  }
+  const defaultValue: TestObject = { str: "", num: 0, bool: false };
+
+  const blueprint: ObjectBlueprint<TestObject> = {
+    str: map().toAttribute("").mandatory().asString(),
+    num: map().toElement("").mandatory().asNumber(),
+    bool: map().toNode("").mandatory().asBoolean(),
+  };
+
+  test("regular mapping", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .asArray()
+        .ofObjects(blueprint)
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<TestObject[] | undefined>>();
+  });
+
+  test("mandatory mapping", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .mandatory()
+        .asArray()
+        .ofObjects(blueprint)
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<TestObject[]>>();
+  });
+
+  test("regular mapping with default value", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .asArray()
+        .ofObjects(blueprint)
+        .withDefault([defaultValue])
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<TestObject[]>>();
+  });
+
+  test("mandatory mapping with default value", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .mandatory()
+        .asArray()
+        .ofObjects(blueprint)
+        .withDefault([defaultValue])
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<TestObject[]>>();
+  });
+
+  test("regular mapping with conversion", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .asArray()
+        .ofObjects(blueprint)
+        .withConversion(toJson)
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<string | undefined>>();
+  });
+
+  test("mandatory mapping with conversion", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .mandatory()
+        .asArray()
+        .ofObjects(blueprint)
+        .withConversion(toJson)
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<string>>();
+  });
+
+  test("regular mapping with conversion and default value", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .asArray()
+        .ofObjects(blueprint)
+        .withConversion(toJson)
+        .withDefault("")
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<string>>();
+  });
+
+  test("mandatory mapping with conversion and default value", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .mandatory()
+        .asArray()
+        .ofObjects(blueprint)
+        .withConversion(toJson)
+        .withDefault("")
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<string>>();
+  });
+
+  test("regular mapping with setting conversion callback after default value", () => {
+    expectTypeOf(
+      map()
+        .toElementsArray("")
+        .asArray()
+        .ofObjects(blueprint)
+        .withDefault([defaultValue])
+        .withConversion(toJson)
+        .createNodeDataExtractor()
+    ).toEqualTypeOf<SingleNodeDataExtractorFn<string | undefined>>();
+  });
+});
